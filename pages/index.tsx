c1bd54503c22e6b59661586c872cb52944aa27a9
Gisela <gisela.difini@4all.com>
2021-03-14 14:43:49 -0300

    PDV-000 | remove commments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from 'next'
-// import Link from 'next/link'
 import Layout from '../components/Layout'
 
 import Cart from '../components/Cart'
@@ -9,16 +8,6 @@ import Products from '../components/Products'
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | e-PDV">
-      {/* <ul className="card-list">
-        <li>
-          <Link href="/use-shopping-cart">
-            <a className="card cart-style-background">
-              <h2 className="bottom">Use Shopping Cart</h2>
-              <img src="/use-shopping-cart.png" />
-            </a>
-          </Link>
-        </li>
-      </ul> */}
       <Cart>
         <Products />
         <CartSummary />
