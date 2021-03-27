d50c6e147637f15fe85e1c3f3d1253774284550a
Gisela <gisela.difini@4all.com>
2021-03-27 14:17:11 -0300

    PDV-000 | clear unused code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
@@ -7,7 +7,7 @@ import { ThemedApp } from '../styles';
 
 export const cache = createCache({ key: 'css', prepend: true });
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): ReactNode {
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
