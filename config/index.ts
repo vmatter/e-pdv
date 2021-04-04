d386e9f0c1258bff163466e9cd0d71c6d4c29fff
Gisela <gisela.difini@4all.com>
2021-04-04 14:53:02 -0300

    PDV-157 | initial items style

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
-export const CURRENCY = 'usd';
+export const CURRENCY = 'BRL';
 // Set your amount limits: Use float for decimal currencies and
 // Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
 export const MIN_AMOUNT = 10.0;
