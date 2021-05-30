87b37f05a67268c498fac595d06ae5c01fc73de4
Gisela <gisela.mdi@gmail.com>
2021-05-30 09:48:12 -0300

    PDV-223 | removed unused import

--- components/Dialog/index.tsx
+++ components/Dialog/index.tsx
@@ -1,7 +1,6 @@
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
