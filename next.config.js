e5d7d6da51230d41a12736397374e5842254c2fa
Gisela Difini <>
2021-06-06 19:47:47 -0300

    PDV-000 | update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
     API_URL: 'https://epdv-api.herokuapp.com/',
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      'pk_test_51IwoaUI3DLjCBNrBu4JMlNkoYYi7ueqXRxLnKHdsp8F25h0zAjVcHmt47CxBRwaAVcYHrpqQkmxstXLDw1lQsIaR00tqOzKtUK',
   },
 };
